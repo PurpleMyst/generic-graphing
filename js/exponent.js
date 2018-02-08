47b16de07cbde28f4d644dfbe5b7cc696708f676
Akuli <akuviljanen17@gmail.com>
2018-02-08 23:24:28 +0200

    add logarithm graph

--- js/exponent.js
+++ js/exponent.js
@@ -1,6 +1,8 @@
 /* jshint esnext: true, browser: true */
 
 function getExponentEquationString(base) {
+  // if base == 1 or base == 0, it's possible to graph y=base^x but a logarithm
+  // with that base does not exist
   var result = "y = " + GraphUtils.parenthesize(base.toString()) + GraphUtils.superscript("x");
   if ((0 < base && base < 1) || (base > 1)) {
     result += "\nx = log" + GraphUtils.subscript(base.toString()) + "(y)";
